d4e4b5688f3c46bca97153a82576744a8c490261
pratheek157qwds <154472124+pratheek157qwds@users.noreply.github.com>
2025-04-08 19:39:32 +0530

    Update adsenseConfig.ts

--- src/adsenseConfig.ts
+++ src/adsenseConfig.ts
@@ -1,5 +1,5 @@
 export const adSenseConfig = {
-    client: 'ca-pub-YOUR_AD_CLIENT_ID', // Replace with your AdSense client ID
-    slot: 'YOUR_AD_SLOT_ID', // Replace with your AdSense slot ID
+    client: 'ca-pub-YOUR_AD_CLIENT_ID',
+    slot: 'YOUR_AD_SLOT_ID',
   };
   
